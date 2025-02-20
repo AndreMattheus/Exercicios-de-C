d41500d8b59e44be14e0572b36bad6b7394ec613
Matheus André <Andre.Mattheusc@gmail.com>
2025-02-19 22:39:26 -0300

    Add Exemplo Somatorio

---
+++ exemplosomatorio.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(){
+    int somatorio, numero;
+    somatorio = 0;
+
+    scanf("%d", &numero);
+
+    while(numero > 0){
+        somatorio += numero;
+        scanf("%d", &numero);
+    }
+
+    printf("A soma foi: %d\n", somatorio);
+    return 0;
+}
